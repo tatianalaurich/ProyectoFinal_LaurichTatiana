e51eca332118aa066aea907c335c77f545f93a5e
tatiana <tatianalaurich@hotmail.com>
2025-06-26 19:23:04 -0300

    hola

--- js/main.js
+++ js/main.js
@@ -4,30 +4,30 @@ const productos = [
     { nombre: "Ekos Tukuma", tipo: "amaderado" },
     { nombre: "Ekos Pitanga", tipo: "cítrico" },
     { nombre: "Essencial Exclusivo", tipo: "intenso" }
-];
+]
 
 function recomendarProducto(preferencia) {
-    let recomendados = [];
+    let recomendados = []
     for (let producto of productos) {
         if (producto.tipo.toLowerCase() === preferencia.toLowerCase()) {
-        recomendados.push(producto.nombre);
+        recomendados.push(producto.nombre)
         }
     }
         if (recomendados.length > 0) {
-    alert("Te recomendamos: " + recomendados.join(", "));
+    alert("Te recomendamos: " + recomendados.join(", "))
     } 
         else {
-    alert("No tenemos productos para esa preferencia. Probá con: cremoso, fresco, amaderado, cítrico o intenso.");
+    alert("No tenemos productos para esa preferencia. Probá con: cremoso, fresco, amaderado, cítrico o intenso.")
     }
 }
 //empieza el simulador
-alert("Bienvenida/o al recomendador de productos Natura");
+alert("Bienvenida/o al recomendador de productos Natura")
     let seguir = true;
     while (seguir) {
-        let gusto = prompt("¿Qué estilo de fragancia te gusta? (cremoso, fresco, amaderado, cítrico o intenso)");
+        let gusto = prompt("¿Qué estilo de fragancia te gusta? (cremoso, fresco, amaderado, cítrico o intenso)")
         if (gusto) {
-            recomendarProducto(gusto);
+            recomendarProducto(gusto)
         }
-        seguir = confirm("¿Querés hacer otra recomendación?");
-        console.log("Gracias por usar el simulador de productos Natura.");
+        seguir = confirm("¿Querés hacer otra recomendación?")
+        console.log("Gracias por usar el simulador de productos Natura.")
 }
