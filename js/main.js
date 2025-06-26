9f310b2561f18e6ff307fc38fab5c95dcd0008cf
tatiana <tatianalaurich@hotmail.com>
2025-06-26 19:11:05 -0300

    second

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const productos = [
+    { nombre: "Ekos Castaña", tipo: "cremoso" },
+    { nombre: "Ekos Açaí", tipo: "fresco" },
+    { nombre: "Ekos Tukuma", tipo: "amaderado" },
+    { nombre: "Ekos Pitanga", tipo: "cítrico" },
+    { nombre: "Essencial Exclusivo", tipo: "intenso" }
+];
+
+function recomendarProducto(preferencia) {
+    let recomendados = [];
+    for (let producto of productos) {
+        if (producto.tipo.toLowerCase() === preferencia.toLowerCase()) {
+        recomendados.push(producto.nombre);
+        }
+    }
+        if (recomendados.length > 0) {
+    alert("Te recomendamos: " + recomendados.join(", "));
+    } 
+        else {
+    alert("No tenemos productos para esa preferencia. Probá con: cremoso, fresco, amaderado, cítrico o intenso.");
+    }
+}
+//empieza el simulador
+alert("Bienvenida/o al recomendador de productos Natura");
+    let seguir = true;
+    while (seguir) {
+        let gusto = prompt("¿Qué estilo de fragancia te gusta? (cremoso, fresco, amaderado, cítrico o intenso)");
+        if (gusto) {
+            recomendarProducto(gusto);
+        }
+        seguir = confirm("¿Querés hacer otra recomendación?");
+        console.log("Gracias por usar el simulador de productos Natura.");
+}
